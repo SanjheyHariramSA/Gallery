3780a376af05955c15210b19d9449abfe755c506
SANJHEY HARIRAM SA <85211820+SanjheyHariramSA@users.noreply.github.com>
2022-05-20 09:35:45 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,13 @@
 import * as firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
-
-var firebaseConfig = {
-  apiKey: "AIzaSyDLmnoBvnjOQ-AaJgDw1DnVBiTETbcp8LI",
-  authDomain: "the-net-ninja-sandbox.firebaseapp.com",
-  databaseURL: "https://the-net-ninja-sandbox.firebaseio.com",
-  projectId: "the-net-ninja-sandbox",
-  storageBucket: "the-net-ninja-sandbox.appspot.com",
-  messagingSenderId: "485942827092",
-  appId: "1:485942827092:web:1811d9d8f1f5fabcd5b5c1"
+const firebaseConfig = {
+  apiKey: "AIzaSyD3GD-UXQb696GUneM1dlPxUFAaZQ1N3EE",
+  authDomain: "gallery-8266f.firebaseapp.com",
+  projectId: "gallery-8266f",
+  storageBucket: "gallery-8266f.appspot.com",
+  messagingSenderId: "176649955991",
+  appId: "1:176649955991:web:d98b567ea2cd0124b317d2"
 };
 
 // Initialize Firebase
